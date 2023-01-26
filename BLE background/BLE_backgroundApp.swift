84a6a8f5dbbe7cb8d0bfca3fbfc27f3968c8c91f
Per Friis <per.friis@friisconsult.com>
2023-01-26 13:27:23 +0100

    Initial Commit

---
+++ BLE background/BLE_backgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_backgroundApp.swift
+//  BLE background
+//
+//  Created by Per Friis on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BLE_backgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
